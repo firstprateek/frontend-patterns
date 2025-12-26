f2ea8bafd12c090ae23312e5c4bbf0c8f701698a
Prateek Joshi <lastprateek@gmail.com>
2025-12-26 02:01:37 -0500

    delete fn

--- file-explorer/index.js
+++ file-explorer/index.js
@@ -1,186 +1,241 @@
 import { data as fileData } from './mockFileData.js';
 
-// Older approach
-/*
-function flattenTree(nodes) {
-	const stack = nodes.map(node => [node, 0, true]).reverse(); // [node, depth, isParentExpanded]
-	const result = [];
+const DEFAULT_ITEM_HEIGHT = 30;
 
-	while (stack.length > 0) {
-		const [{ id, name, children = [] }, depth, isParentExpanded] = stack.pop();
-		result.push({
-			id,
-			name,
-			depth,
-			isVisible: isParentExpanded,
-			isExpanded: false,
-			isFolder: children.length > 0
-		});
+class FileExplorer {
+	#data = null;
+	#nodeMap = new Map();
+	#rootIds = [];
+	#visibleItems = [];
+	#container = null;
+	#spacer = document.createElement('div');
+	#buffer = 5;
+	#poolSize = 2 * this.#buffer;
+	#pool = [];
+	#scrollListener = null;
+	#clickListener = null;
 
-		for (let i = children.length - 1; i >= 0; i--) {
-			stack.push([children[i], depth + 1, false]);
-		}
+	constructor(containerId, { data, hasFixedHeight = true }) {
+		this.#data = data;
+		this.#rootIds = data.map(rootEntry => rootEntry.id);
+		this.#container = document.getElementById(containerId);
+		this.#spacer = document.createElement('div');
 	}
 
-	return result;
-}
-*/
-(function() {
-	// Input Tree -> | Normalizer | -> NodeMap -> | Projection | -> visibleList -> | Virtualizer | -> virtualList
-	// Model
+	startup() {
+		this.#buildNodeMap();
+		this.#updateVisibleItems();
+		this.#setupDOM();
+		this.#setupListeners();
+		this.#render();
+	}
 
-	let state = {
-	   nodeMap: new Map(), // Store
-	   rootIds: [], 
-	   visibleItems: [] // Flattened list used for virtualization
-	};
-
-	function buildNodeMap(nodes) {
+	#buildNodeMap() {
+		const fileData = this.#data;
 		const map = new Map();
-		const stack = nodes.map(node => ({ ...node, parentId: null }));
+		const stack = fileData.map(data => ({ data, parentId: null }));
 
 		while (stack.length) {
-			const { id, name, parentId, children } = stack.pop();
-			map.set(id, {
-				id,
-				name,
-				childrenIds: (children || []).map(c => c.id),
-				isExpanded: true,
+			const { data, parentId } = stack.pop();
+			map.set(data.id, {
+				name: data.name,
 				parentId,
-				depth: 0
+				childrenIds: (data.children || []).map(child => child.id),
+				isExpanded: false,
+				isDir: !!data.children
 			});
 
-			if (children) {
-				children.forEach(child => {
-					stack.push({ ...child, parentId: id });
-				});
-			}
-		}
-
-		return map;
-	}
-
-	// Projection function
-	function getVisibleItems(rootIds, nodeMap) {
-		const result = [];
-		const stack = rootIds.slice().reverse().map(id => [id, 0]);
-
-		while (stack.length) {
-			const [id, depth] = stack.pop();
-			const node = nodeMap.get(id);
-
-			result.push({
-				id,
-				name: node.name,
-				isExpanded: node.isExpanded,
-				depth
-			});
-
-			if (node.isExpanded) {
-				node.childrenIds.toReversed().forEach(childId => {
-					stack.push([childId, 1 + depth]);
+			if (data.children) {
+				data.children.forEach(child => {
+					stack.push({ data: child, parentId: data.id });
 				})
 			}
 		}
 
-		return result;
+		this.#nodeMap = map;
 	}
 
-	// Controller
-	function toggleNode(id) {
-	    const node = state.nodeMap.get(id);
-	    if (node) {
-	        node.isExpanded = !node.isExpanded;
-	        state.visibleItems = getVisibleItems(state.rootIds, state.nodeMap);
-	        renderVirtualList(); 
-	    }
+	#updateVisibleItems() {
+		const nodeMap = this.#nodeMap;
+		const stack = this.#rootIds.map(id => ({ id, depth: 0 })).reverse();
+		const result = [];
+
+		while (stack.length) {
+			const { id, depth } = stack.pop();
+			const node = nodeMap.get(id);
+
+			result.push({ id, depth, isExpanded: node.isExpanded, name: node.name, isDir: node.isDir });
+
+			if (node.isDir && node.childrenIds.length && node.isExpanded) {
+				node.childrenIds.toReversed().forEach(id => stack.push({id, depth: depth + 1}));
+			}
+		}
+
+		this.#visibleItems = result;
 	}
 
-	// Setup
-	state.nodeMap = buildNodeMap(fileData);
-	state.rootIds = Array(100).fill(0).map((_, index) => index + 1);
-	state.visibleItems = getVisibleItems(state.rootIds, state.nodeMap);
+	#setupDOM() {
+		// Setup container to be scrollable and allow for absolute positioning for children
+		this.#container.style.position = 'relative';
+		this.#container.style.overflowY = 'auto';
 
-	// View
-	const itemHeight = 30;
+		const containerHeight = this.#container.clientHeight;
+		const numberOfVisibleItems = Math.ceil(containerHeight / DEFAULT_ITEM_HEIGHT);
 
-	const container = document.getElementById('container');
-	container.style.position = 'relative';
-	container.style.overflowY = 'auto';
+		// Add the spacer element
+		this.#spacer.style.width = '100%';
+		this.#container.appendChild(this.#spacer);
 
-	const spacer = document.createElement('div');
-	container.appendChild(spacer);
+		// Build the DOM pool
+		this.#poolSize = numberOfVisibleItems + (this.#buffer * 2);
+		for (let i = 0; i < this.#poolSize; i++) {
+			const domNode = document.createElement('div');
+			domNode.style.height = `${DEFAULT_ITEM_HEIGHT}px`;
+			domNode.style.position = 'absolute';
+			domNode.style.top = 0;
+			domNode.style.width = '100%';
+			domNode.style.cursor = 'pointer';
+			domNode.style.willChange = 'transform';
 
-	const buffer = 5;
-	const containerHeight = container.clientHeight;
-	const numberOfVisibleItems = Math.ceil(containerHeight / itemHeight);
-	const poolSize = numberOfVisibleItems + (buffer * 2);
+			const deleteBtn = document.createElement('button');
+			deleteBtn.style.backgroundColor = 'transparent';
+			deleteBtn.style.border = '1px solid #EEE';
+			deleteBtn.innerText = 'x';
 
-	const pool = [];
-	for (let i = 0; i < poolSize; i++) {
-		const item = document.createElement('div');
-		item.style.position = 'absolute';
-		item.style.height = `${itemHeight}px`;
-		item.style.width = '100%';
-		item.style.top = '0';
-		item.style.willChange = 'transform';
+			const span = document.createElement('span');
+			domNode.appendChild(span);
+			domNode.appendChild(deleteBtn);
 
-		container.appendChild(item);
-		pool.push(item);
+			this.#pool.push(domNode);
+			this.#container.appendChild(domNode);
+		}
 	}
 
-	function renderVirtualList() {
-		const scrollTop = container.scrollTop;
-		const totalItems = state.visibleItems.length;
+	#setupListeners() {
+		let isTicking = false;
+		this.#scrollListener = (event) => {
+			if (!isTicking) {
+				window.requestAnimationFrame(() => {
+					this.#render();
+					isTicking = false;
+				});
+				isTicking = true;
+			}
+		};
+		this.#container.addEventListener('scroll', this.#scrollListener);
 
-		// Dynamically adjust the height based on the latest visible Items
-		spacer.style.height = `${itemHeight * state.visibleItems.length}px`;
 
-		const indexStart = Math.floor(scrollTop / itemHeight);
-		const renderingStart = Math.max(indexStart - buffer, 0);
+		this.#clickListener = (event) => {
+			const divParent = event.target.closest('div');
+			const deleteParent = event.target.closest('button');
+			if (divParent) {
+				const id = divParent.dataset.id;
 
-		for (let i = 0; i < poolSize; i++) {
-			const dataIndex = renderingStart + i;
-			const domNode = pool[i];
+				if (deleteParent) {
+					this.deleteNode(parseInt(id, 10));
+				} else {
+					this.toggleNodeIsExpanded(parseInt(id, 10));
+				}
+			}
+		}
 
-			if (dataIndex < totalItems) {
-				const itemData = state.visibleItems[dataIndex];
-				domNode.style.visibility = 'block';
-				domNode.style.transform = `translateY(${dataIndex * itemHeight}px)`;
+		this.#container.addEventListener('click', this.#clickListener);
+	}
 
-				domNode.innerText = `${itemData.isExpanded ? '▼' : '▶'} ${itemData.name}`;
-	            domNode.style.paddingLeft = `${itemData.depth * 20 + 10}px`;
+	#removeEventListeners() {
+		this.#container.removeEventListeners('scroll', this.#scrollListener);
+		this.#container.removeEventListeners('click', this.#clickListener);
+	}
 
-	            domNode.dataset.id = itemData.id;
+	toggleNodeIsExpanded(id) {
+		const node = this.#nodeMap.get(id);
+		if (node && node.isDir) {
+			node.isExpanded = !node.isExpanded;
+			this.#updateVisibleItems();
+			this.#render();
+		}
+	}
+
+	deleteNode(id) {
+		const node = this.#nodeMap.get(id);
+		if (node) {
+			// Mark for deletion
+			node.isDeleting = true;
+			this.#updateVisibleItems();
+			this.#render();
+
+			setTimeout(() => {
+				// Mimic async call
+				const random = Math.floor(Math.random() * 10) + 1;
+				if (random > 7) {
+					// Show toast to user to inform deletion went wrong
+
+					delete node.isDeleting;
+					this.#updateVisibleItems();
+					this.#render();
+				} else {
+					this.#nodeMap.delete(id);
+					node.childrenIds.forEach(id => this.#nodeMap.delete(id));
+				}
+
+			}, 2000);
+		}
+	}
+
+	#render() {
+		const containerHeight = this.#container.clientHeight;
+		const scrollTop = this.#container.scrollTop;
+
+		// Adjust spacer height to set the scroll position correctly
+		this.#spacer.style.height = `${this.#visibleItems.length * DEFAULT_ITEM_HEIGHT}px`;
+
+		// Find the items that are currently viewable in the view port
+		const startIndex = Math.floor(scrollTop / DEFAULT_ITEM_HEIGHT);
+
+		// start rendering from 5 items above
+		const renderStartIdx = Math.max(startIndex - this.#buffer, 0);
+
+		for (let i = 0; i < this.#poolSize; i++) {
+			const itemIdx = renderStartIdx + i;
+			const domNode = this.#pool[i];
+
+			if (itemIdx < this.#visibleItems.length) {
+				const item = this.#visibleItems[itemIdx];
+
+				domNode.style.display = 'block';
+				domNode.style.transform = `translateY(${DEFAULT_ITEM_HEIGHT * itemIdx}px)`;
+				domNode.dataset.id = item.id;
+
+				let spanText = '';
+				if (!item.isDir) {
+					spanText = item.name;
+				} else {
+					spanText = `${item.isExpanded ? '▼' : '▶'} ${item.name}`
+				}
+
+				const span = domNode.firstElementChild;
+				const deleteBtn = domNode.lastElementChild;
+
+				span.textContent = spanText;
+
+				domNode.style.paddingLeft = `${10 + item.depth * 20}px`;
 			} else {
 				domNode.style.display = 'none';
 			}
-
 		}
-
 	}
 
-	renderVirtualList();
+	destroy() {
+		this.#nodeMap = null;
+		this.#rootIds = null;
+		this.#visibleItems = null;
+		this.#removeEventListeners();
+		this.#spacer.remove();
+		this.#pool.forEach(domNode => domNode.remove());
+		this.#container.innerHTML = '';
+		this.#container = null;
+	}
+}
 
-	let isTicking = false;
-	// scroll listener
-	container.addEventListener('scroll', () => {
-		if (!isTicking) {
-			window.requestAnimationFrame(() => {
-				renderVirtualList();
-	            isTicking = false;
-			});
-			isTicking = true;
-		}
-	});
-
-	// delegate click event handler
-
-	container.addEventListener('click', (e) => {
-	    // Only care if we clicked a row
-	    const row = e.target.closest('div'); // or whatever class you gave the rows
-	    if (row && row.dataset.id) {
-	        toggleNode(parseInt(row.dataset.id));
-	    }
-	});
-}())
+const fileExplorer = new FileExplorer('container', { data: fileData });
+fileExplorer.startup();
